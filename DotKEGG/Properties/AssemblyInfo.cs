9452be466372a8ca02d69e5b97d699333d77fbdb
Dan Vicarel <Daniel.Vicarel@amtrustgroup.com>
2017-03-21 17:51:28 -0400

    Added a documentation project!

--- DotKEGG/Properties/AssemblyInfo.cs
+++ DotKEGG/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DotKegg")]
+[assembly: AssemblyTitle("DotKEGG")]
 [assembly: AssemblyDescription("A .NET wrapper for the Kyoto Encyclopedia of Genes and Genomes REST API")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Danware Creations, LLC")]
-[assembly: AssemblyProduct("DotKegg")]
+[assembly: AssemblyProduct("DotKEGG")]
 [assembly: AssemblyCopyright("Copyright ©  2017 Danware Creations, LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
