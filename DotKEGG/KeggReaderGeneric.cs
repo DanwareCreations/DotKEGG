6d7f97bdc479feb922f1f78b452443c213b4d0d1
Dan Vicarel <Daniel.Vicarel@amtrustgroup.com>
2017-04-19 10:18:38 -0400

    Added a generic version of the KeggReader that the strongly typed database classes can use

---
+++ DotKEGG/KeggReaderGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace DotKEGG {
+    
+    public sealed class KeggReader<ID> : KeggReader where ID : KeggId {
+
+        internal KeggReader(Stream input) : base(input) { }
+
+        public new ID Read() => base.Read() as ID;
+        public int ReadBlock(ID[] buffer, int index, int count) => base.ReadBlock(buffer, index, count);
+        public new ID[] ReadToEnd() => base.ReadToEnd().Cast<ID>().ToArray();
+
+    }
+
+}
